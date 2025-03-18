ffac2a9b10436a381ffa50791492905f8c8612f9
hellosiyam <hellosiyambd@gmail.com>
2025-03-19 05:28:25 +0600

    simple-firebase-Authincation

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -6,17 +6,24 @@ import { auth } from '../../firebase.init';
 const Signup = () => {
 
     const [errorMassage, setErrorMassage] = useState('')
-
+    const [successMassage, setSuccessMassage] = useState(false)
     const handelSignup = (e) => {
         e.preventDefault()
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
         setErrorMassage('');
+        setSuccessMassage(false)
+
+        if (password.length < 6) {
+            setErrorMassage('Password Must be less then 6 character')
+            return
+        }
 
         createUserWithEmailAndPassword(auth, email, password)
             .then((result) => {
                 console.log(result.user);
+                setSuccessMassage(true)
             })
             .catch(error => {
                 console.log('ERROR', error);
@@ -33,14 +40,17 @@ const Signup = () => {
                     <form onSubmit={handelSignup} className="card-body">
                         <fieldset className="fieldset">
                             <label className="fieldset-label">Email</label>
-                            <input type="email" name='email' className="input" placeholder="Email" />
+                            <input type="email" name='email' className="input" placeholder="Email" required/>
                             <label className="fieldset-label">Password</label>
-                            <input type="password" name='password' className="input" placeholder="Password" />
+                            <input type="password" name='password' className="input" placeholder="Password" required/>
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Login</button>
                         </fieldset>
                         {
-                            errorMassage && <p className='text-red-500'>Email Already Use</p>
+                            errorMassage && <p className='text-red-600'>{errorMassage}</p>
+                        }
+                        {
+                            successMassage && <p className='text-green-600'>Sign Up Successfully</p>
                         }
                     </form>
                 </div>
