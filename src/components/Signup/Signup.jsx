5b2b0135365035368863f057fe2935ad534768f4
hellosiyam <hellosiyambd@gmail.com>
2025-03-19 17:44:23 +0600

    simple-firebase-Authincation

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -2,59 +2,85 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
+import { FaEye, FaEyeSlash } from 'react-icons/fa6';
 
 const Signup = () => {
 
     const [errorMassage, setErrorMassage] = useState('')
     const [successMassage, setSuccessMassage] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
+
     const handelSignup = (e) => {
         e.preventDefault()
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        const terms = e.target.terms.checked
+        console.log(email, password, terms);
         setErrorMassage('');
         setSuccessMassage(false)
 
-        if (password.length < 6) {
-            setErrorMassage('Password Should be more then 6 character')
-            return
+        if (!terms) {
+            setErrorMassage('Pleas Accept Our Terms & Conditions');
+            return;
         }
 
-        createUserWithEmailAndPassword(auth, email, password)
+        if (password.length < 6) {
+            setErrorMassage('Password Should be more then 6 character');
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth, email, password,)
             .then((result) => {
                 console.log(result.user);
                 setSuccessMassage(true)
             })
             .catch(error => {
                 console.log('ERROR', error);
-                setErrorMassage(error.message)
+                setErrorMassage('This Email Already Use')
             })
 
     }
 
     return (
-        <div className='max-w-5xl mx-auto text-center'>
-            <h2 className='text-3xl font-medium mx-4'>Sign in Form</h2>
-            <div className='max-w-5xl mx-auto my-5'>
-                <div className="card w-full max-w-sm shrink-0 shadow-2xl mx-auto">
-                    <form onSubmit={handelSignup} className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="fieldset-label">Email</label>
-                            <input type="email" name='email' className="input" placeholder="Email" required/>
-                            <label className="fieldset-label">Password</label>
-                            <input type="password" name='password' className="input" placeholder="Password" required/>
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
-                        {
-                            errorMassage && <p className='text-red-600'>{errorMassage}</p>
-                        }
-                        {
-                            successMassage && <p className='text-green-600'>Sign Up Successfully</p>
-                        }
-                    </form>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+            <h2 className='text-3xl font-medium mx-4 text-center'>Sign in Form</h2>
+            <form className="card-body" onSubmit={handelSignup}>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text my-2">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                 </div>
-            </div>
+                <div className="form-control relative">
+                    <label className="label">
+                        <span className="label-text my-2">Password</span>
+                    </label>
+                    <input type={showPassword ? 'text' : 'password'} name='password' placeholder="password" className="input input-bordered" required />
+                    <button onClick={() => setShowPassword(!showPassword)} className='absolute right-6 top-12 text-lg'>
+                        {
+                            showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+                        }
+                    </button>
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover mt-4">Forgot password?</a>
+                    </label>
+                    <div className="form-control mt-4">
+                        <label className="label cursor-pointer">
+                            <input type="checkbox" name='terms' className="checkbox" />
+                            <span className="label-text">Accept Our Terms And Condition.</span>
+                        </label>
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary w-full">Login</button>
+                </div>
+                {
+                    errorMassage && <p className='text-red-600 text-center'>{errorMassage}</p>
+                }
+                {
+                    successMassage && <p className='text-green-600 text-center'>Sign Up Successfully</p>
+                }
+            </form>
         </div>
     );
 };
